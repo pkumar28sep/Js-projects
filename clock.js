1e2ca0e5dc98128197ef27acc694b113bb578962
Prabhat kumar <55682189+pkumar28sep@users.noreply.github.com>
2021-09-18 00:18:24 +0530

    clock
    Alarm Clock

---
+++ clock.js
@@ -0,0 +1,19 @@
+console.log("This is my clock.js");
+
+function updateClock(){
+    let currentTime=new Date();
+    let currentHour=currentTime.getHours();
+    let currentMinutes=currentTime.getMinutes();
+    let currentSeconds=currentTime.getSeconds();
+
+    currentMinutes=(currentMinutes<10?"0":"")+currentMinutes;
+    currentSeconds=(currentSeconds<10?"0":"")+currentSeconds;
+    currentHour=(currentHour<10?"0":"")+currentHour;
+
+    let timeOfDay=(currentHour<12)?"AM":"PM";
+    currentHour=(currentHour>12)?currentHour-12:currentHour;
+    currentHour=(currentHour==0)?12:currentHour;
+    let currentTimeStr=currentHour+":"+currentMinutes+":"+currentSeconds+" "+timeOfDay;
+
+    document.getElementById("clock").innerHTML=currentTimeStr;
+}
